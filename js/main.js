9ae229f88db62be7bfe67a32545aa282167739c2
Andrew Miller <andrew.miller0810@knights.ucf.edu>
2022-03-08 14:04:10 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,17 +1,20 @@
 // main file hopefully
 
 var moving  = false;
+/*
 
 var canvas  = document.getElementById('$canvas');
 var context = canvas.getContext('2d');
 
 var sprites = {
     Idle = document.getElementById("player$idle"),
-} 
+};
+
+*/
 
 function moveSprite(event) {
     var key = event.key;
-    if (key != 'a' && key != 'd'){
+    if (key != 'a' && key != 'd') {
         alert(key);
         return;
     }
