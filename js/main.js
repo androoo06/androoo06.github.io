f483529afeee510bc12f0842ded970abac891b95
Andrew Miller <andrew.miller0810@knights.ucf.edu>
2022-03-07 22:51:38 -0500

    t

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
 // main file hopefully
-document.addEventListener('keydown', function(event) {
-   alert(event.key);
-});
+
+function moveSprite(event) {
+    var key = event.key;
+    if (key != 'w' && key != 'a' && key == 's' && key != 'd'){
+        return;
+    }
+
+    
+
+}
+
+document.addEventListener('keydown', moveSprite);
