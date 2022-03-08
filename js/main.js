1aa59e164a166e09c195ff45d4a55453c54715ae
Andrew Miller <andrew.miller0810@knights.ucf.edu>
2022-03-08 14:34:06 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,19 +2,23 @@
 
 var moving   = false;
 
-var canvas   = document.getElementById('$canvas');
-var context  = canvas.getContext('2d');
+var canvas   = document.getElementById("$canvas");
+var context  = canvas.getContext("2d");
 
 var sprites  = {};
 sprites.Idle = document.getElementById("player$idle");
 
-sprites.Idle.onload = function() {
-    canvas.drawImage(sprites.Idle, 128, 128);
-}
+sprites.Idle.addEventListener('load', function(){
+    //
+}, false);
+
+canvas.addEventListener('mouseenter', function(){
+    alert("hello");
+}, false);
 
 function moveSprite(event) {
     var key = event.key;
-    if (key != 'a' && key != 'd') {
+    if (key != "a" && key != "d") {
         alert(">" + key + "<");
         return;
     }
