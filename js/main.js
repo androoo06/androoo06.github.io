dece6270aea4c267f0c991fc7ad2dab558f18491
Andrew Miller <andrew.miller0810@knights.ucf.edu>
2022-03-07 23:55:59 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,12 +2,29 @@
 
 var moving  = false;
 
+var canvas  = document.querySelector('canvas');
+var context = canvas.getContext('2d');
+
 var sprites = {
     Idle = new Image(128, 128),
 };
 
 sprites.Idle.src         = "sprites/Person.png";
 sprites.Idle.crossOrigin = true
+sprites.Idle.onload = function() {
+    context.drawImage(
+        image,
+        0,
+        0,
+        image.width,
+        image.height,
+        0,
+        0,
+        canvas.width,
+        canvas.height
+    );
+};
+
 
 function moveSprite(event) {
     var key = event.key;
