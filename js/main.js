c3914c5fcb3b18a35595ba5e11dc5928dc71d480
Andrew Miller <andrew.miller0810@knights.ucf.edu>
2022-03-07 23:51:30 -0500

    moved image to main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,21 @@
 // main file hopefully
 
+var moving  = false;
+
+var sprites = {
+    Idle = new Image(128, 128),
+};
+
+sprites.Idle.src         = "sprites/Person.png";
+sprites.Idle.crossOrigin = true
+
 function moveSprite(event) {
     var key = event.key;
     if (key != 'w' && key != 'a' && key == 's' && key != 'd'){
         return;
     }
 
-    
+
 
 }
 
