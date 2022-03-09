24187309d4c79710a3c145ca20ae36630dd14435
Andrew Miller <andrew.miller0810@knights.ucf.edu>
2022-03-09 14:56:14 -0500

    test

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 import * as sprites from "./modules/spriteController.js";
-//var sprites = require("./modules/spriteController");
 
 document.addEventListener('keydown', sprites.moveSprite);
 document.addEventListener('keyup',   sprites.stopSprite);
--- js/modules/spriteController.js
+++ js/modules/spriteController.js
@@ -15,63 +15,63 @@ var duration  = 0; //useDiration for how long the current sprite image has been 
 var leftWall  = 0;
 var rightWall = 500;
 
-var player    = document.getElementById("$player$");
+export var player = document.getElementById("$player$");
 
 function getNextSprite(curr) {
-  var _curr       = curr.search("sprites/");
-  var current     = curr.substring(_curr);
+    var _curr       = curr.search("sprites/");
+    var current     = curr.substring(_curr);
 
-  if ((current == "sprites/Idle.png") || (current == "sprites/Walk2.png")){
-    return "sprites/Walk.png";
-  } else if (current == "sprites/Walk.png"){
-    return "sprites/Walk2.png";
-  }
+    if (current == "sprites/Idle.png") {
+        return "sprites/walk0.jpg";
+    }
+
+    var walkNum    = parseInt(current.substring(12));
+    console.log(current.substring(12) + "  " + walkNum);
+    if (walkNum) {
+        var newWalk  = (walkNum+1) % 4;
+        console.log(newWalk);
+        return "sprites/walk"+ (newWalk) +".jpg";
+    }
 }
 
 export function moveSprite(event) {
-  var key = event.key;
-  if ((key != "a" && key != "d" && key != " ")) {
-    return;
-  }
-  if (intervals.length > 0){
-    return;
-  }
-
-  var leftInc = dirs[key];
-
-  var m = setInterval(function () {
-    var newLoc = pLeft + (leftInc*2);
-    duration++;
-
-    if ((newLoc < rightWall) && (newLoc > leftWall)) {
-      player.style.left = newLoc + "px";
-      pLeft = newLoc;
-
-      if (duration % 14 == 0){
-        player.src = getNextSprite(player.src);
-      }
+    var key = event.key;
+    if ((key != "a" && key != "d" && key != " ")) {
+        return;
     }
-  }, 10);
+    if (intervals.length > 0){
+        return;
+    }
 
-  intervals.push(m); // add to intervals
+    var leftInc = dirs[key];
+
+    var m = setInterval(function () {
+        var newLoc = pLeft + (leftInc*2);
+        duration++;
+
+        if ((newLoc < rightWall) && (newLoc > leftWall)) {
+            player.style.left = newLoc + "px";
+            pLeft = newLoc;
+
+            if (duration % 14 == 0){
+                player.src = getNextSprite(player.src);
+            }
+        }
+    }, 10);
+
+    intervals.push(m); // add to intervals
 }
 
 export function stopSprite(event) {
-  var key = event.key;
-  if (key != "a" && key != "d" && key != " ") {
-    return;
-  }
+    var key = event.key;
+    if (key != "a" && key != "d" && key != " ") {
+        return;
+    }
 
-  for (var i = (intervals.length-1); i>-1; i--){
-    clearInterval(intervals[i]);
-    intervals.pop();
-  }
+    for (var i = (intervals.length-1); i>-1; i--){
+        clearInterval(intervals[i]);
+        intervals.pop();
+    }
 
-  player.src = "sprites/Idle.png";
+    player.src = "sprites/Idle.png";
 }
-
-/*
-module.exports = {
-  "moveSprite":moveSprite,
-  "stopSprite":stopSprite
-};*/
