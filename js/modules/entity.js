fa9159765dc270d7f1d36ee72ce54578f6b48efb
Andrew Miller <andrew.miller0810@knights.ucf.edu>
2022-03-10 14:51:02 -0500

    Update entity.js

--- js/modules/entity.js
+++ js/modules/entity.js
@@ -1,36 +1,41 @@
 export class Entity {
+    // private method to update this.boundsBox when properties are changed
+    #drawBoundsBox() {
+        // leftBound, rightBound, bottomBound, topBound
+        this.boundsBox = {
+            "left":    this.pX,
+            "right":  (this.pX + this.width),
+            "bottom": (this.pY + this.height),
+            "top":     this.pY
+        };
+    }
+
     constructor(width, height, x, y) {
         this.pX        = x;
         this.pY        = y;
         this.width     = width;
         this.height    = height;
 
-        this.drawBoundsBox();
-    }
-
-    // private method to update this.boundsBox when properties are changed
-    #drawBoundsBox() {
-        // leftBound, rightBound, bottomBound, topBound
-        this.boundsBox = [this.pX, (this.pX + this.width), (this.pY + this.height), this.pY];
+        this.#drawBoundsBox();
     }
 
     // return whether (x, y) is in object bounds (would cause overlap)
     isInBounds(x, y) {
         var bb = this.boundsBox;
-        return (x >= bb[0]) && (x <= bb[1]) && (y <= bb[2]) && (y >= bb[3]);
+        return (x >= bb.left) && (x <= bb.right) && (y <= bb.bottom) && (y >= bb.top);
     }
 
     // change position to (newX, newY)
     shift(newX, newY) {
         this.pX = newX;
         this.pY = newY;
-        this.drawBoundsBox();
+        this.#drawBoundsBox();
     }
 
     // change size to (width, height)
     resize(width, height) {
         this.width  = width;
         this.height = height;
-        this.drawBoundsBox();
+        this.#drawBoundsBox();
     }
 }
