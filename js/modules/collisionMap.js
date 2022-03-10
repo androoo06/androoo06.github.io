7f4056806b6e4b9c96f89aa02b32817882438bc7
Andrew Miller <andrew.miller0810@knights.ucf.edu>
2022-03-10 14:40:39 -0500

    Update collisionMap.js

--- js/modules/collisionMap.js
+++ js/modules/collisionMap.js
@@ -1,14 +1,14 @@
 ////  Collision Map  ////
 
-import Entity as entity from "./entity.js";
+import {Entity} from "./entity.js";
 
 // canvas stuff [defined like this in all scripts thus far]
 var canvas  = document.getElementById("canvas");
 var ctx     = canvas.getContext("2d");
 
-var _screen = new entity(canvas.width, canvas.height, 0, 0);
+var _screen = new Entity(canvas.width, canvas.height, 0, 0);
 
-var objs    = []
+var objs    = [];
 
 export function boundsCheck(x, y){
     if (!_screen.isInBounds(x, y)) return false;
