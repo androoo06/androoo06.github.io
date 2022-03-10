38a887c907e1290ee8cc53ddc5adf347697251ed
Andrew Miller <andrew.miller0810@knights.ucf.edu>
2022-03-10 14:05:51 -0500

    same

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 ////  MAIN  ////
 
+// canvas exports
+export var canvas = document.getElementById("canvas");
+export var ctx    = canvas.getContext("2d");
+
 // imports
 import * as sprites from "./modules/spriteController.js";
 import * as MAP     from "./modules/collisionMap.js";
-
-// canvas stuff
-export var canvas = document.getElementById("canvas");
-export var ctx    = canvas.getContext("2d");
 
 // events
 document.addEventListener('keyup',   sprites.stopSprite);
