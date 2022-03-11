81396285578c3bd4ab0b32d8fa06bee6f5cadcbf
Andrew Miller <andrew.miller0810@knights.ucf.edu>
2022-03-10 23:44:50 -0500

    fixed movement again

--- js/modules/dailyEntities.js
+++ js/modules/dailyEntities.js
@@ -9,18 +9,19 @@ function _setup(name, src, posX, posY, sizeX, sizeY){
     var _element = new Image();
     _element.src = src;
 
-    console.log("setting up for: "+name+" "+src+" "+posX+" "+posY+" "+sizeX+" "+sizeY);
+    //console.log("setting up for: "+name+" "+src+" "+posX+" "+posY+" "+sizeX+" "+sizeY);
 
     var element = new Entity(posX, posY, sizeX, sizeY, name, _element);
-    _element.addEventListener("load", element.draw);
+    _element.addEventListener("load", function(){
+        element.draw(); // i can't simplify this to "element.draw" ;; tried and it doesn't work the same [[only does if the function is local]]
+    });
 
     return element;
 }
 
 // player [always will be here]
-export var player = _setup("Player", "sprites/Idle.png", 0, (canvas.height - plrDim), plrDim, plrDim);
 export var plrDim = 64; // png should be 200x200 or smaller (to be rendered non-pixely); actual render is 64x64
-
+export var player = _setup("Player", "sprites/Idle.png", 0, (canvas.height - plrDim), plrDim, plrDim);
 
 // daily entities [ syntax: _setup(name, src, posX, posY, sizeX, sizeY); ]
 ///// ------------------------------------------------------------------------------------------------------------------ /////
